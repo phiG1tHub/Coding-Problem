dc01ffb8626dc3e257809877f8384613e9dd9af6
phiG1tHub <phi.business@protonmail.com>
2019-06-23 01:25:20 +0200

    changes

---
+++ Problem_204.java
@@ -0,0 +1,17 @@
+/*
+ * This is your coding interview problem for today.
+ * 
+ * This problem was asked by Amazon.
+ * 
+ * Given a complete binary tree, count the number of nodes in faster than O(n)
+ * time. Recall that a complete binary tree has every level filled except the
+ * last, and the nodes in the last level are filled starting from the left.
+ * 
+ * We will be sending the solution tomorrow, along with tomorrow's question. As
+ * always, feel free to shoot us an email if there's anything we can help with.
+ * 
+ * Have a great day!
+ */
+import java.util.*;
+class Problem_204{
+}
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Coding-Problem
 Practise 
 ## Done Problems 
-+214 - Given an integer n, return the length of the longest consecutive run of 1s in its binary representation. [Sloved] using Brain Kernighan's Algorithm   
++ 214 - Given an integer n, return the length of the longest consecutive run of 1s in its binary representation. [Sloved] using Brain Kernighan's Algorithm
++   
